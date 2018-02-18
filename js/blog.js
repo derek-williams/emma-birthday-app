38b597bf09ffb6ff30c875c631964483b997a2ec
Derek Williams <the.calm.flow@gmail.com>
2018-02-17 16:43:36 -0800

    adding some js to our blog

---
+++ js/blog.js
@@ -0,0 +1,14 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("G9gUmwaT7FAM12QKarClFkU5zO1YTJuXDwf6XyOj", "ZhdgfHeLwSMyVfQhx0BdX88kutTHCUH9bFZV2LMy");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
