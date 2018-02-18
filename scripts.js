a8a680c2a62a52d5eda3e72e663fb8f7217e01ec
Derek Williams <the.calm.flow@gmail.com>
2018-02-17 18:27:53 -0800

    changing folder structure again

--- scripts.js
+++ scripts.js
@@ -74,7 +74,7 @@ function playSong(el){
 
 function playFunnyTime(el){
 	console.log("emmy wants to use her blog");
-	window.location.href="blog/views/blog.html"
+	window.location.href="views/blog.html"
 
 	currentMediaType = "funny"
 
