5bf9ff8fb7a358d27489064d6b5bf4d7a8dfbc19
Derek Williams <the.calm.flow@gmail.com>
2018-02-17 16:51:26 -0800

    changing how parse gets initialized

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ $(function() {
     Parse.$ = jQuery;
  
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("G9gUmwaT7FAM12QKarClFkU5zO1YTJuXDwf6XyOj", "ZhdgfHeLwSMyVfQhx0BdX88kutTHCUH9bFZV2LMy");
+    //Parse.initialize("G9gUmwaT7FAM12QKarClFkU5zO1YTJuXDwf6XyOj", "ZhdgfHeLwSMyVfQhx0BdX88kutTHCUH9bFZV2LMy");
  
     var TestObject = Parse.Object.extend("TestObject");
     var testObject = new TestObject();
